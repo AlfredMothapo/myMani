11496e9e913782441661443b518c6845fafeead8
Alfred Mothapo <mthmas016@myuct.ac.za>
2018-08-15 21:59:03 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const app = require('express')()
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+const Rehive = require("rehive")
+var rehive = null;
+const port = process.env.PORT || 3030
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('*', (req, res) => {
+  res.send('Hello world')
+})
+app.put("*",(req,res) => {
+  res.send(req.body);
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
+
+/**
+ * Rehive login, will go to the controller.
+ */
+function login(phoneNo,password) {
+  return new Promise((resolve,reject)=>{
+    rehive = new Rehive({ apiVersion: 3 })
+  rehive.auth.login({
+      user: phoneNo,
+      company: "1dod1",
+      password: password
+  }).then(function (user) {
+      return resolve(user);
+  }, function (err) {
+      return reject(err)
+  })
+  })
+}
+/**
+ * Checks the user account balance.
+ */
+function checkBalance(cell,pwd){
+  return new Promise((resolve,reject)=>{
+    login(cell,pwd).then((loggedInUser)=>{
+      console.log(JSON.stringify(loggedInUser))
+      return resolve(loggedInUser.user.balance);
+     },(error)=>{
+       //login error
+      return reject(error)
+     })
+  })
+}
